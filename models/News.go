be9c41e4575a702e1a8c955a3c9b3d89236a6b63
somnoynadno <somnoynadno@yandex.ru>
2020-05-17 02:14:00 +0700

    JSON name fastfix

--- models/News.go
+++ models/News.go
@@ -2,6 +2,6 @@ package models
 
 type News struct {
 	BaseModel
-	Title string `json:"text" gorm:"size:512;"`
+	Title string `json:"title" gorm:"size:512;"`
 	Text  string `json:"text" gorm:"size:16384;"`
 }
