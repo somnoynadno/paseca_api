bb0e0f3d8ab75508f2281f2787eeef62e534e427
somnoynadno <somnoynadno@yandex.ru>
2020-06-01 01:03:33 +0700

    Subrouters

--- middleware/Permissions.go
+++ middleware/Permissions.go
@@ -4,30 +4,16 @@ import (
 	"errors"
 	"net/http"
 	u "paseca/utils"
-	"strings"
 )
 
-var CheckPermissions = func(next http.Handler) http.Handler {
+var CheckAdminPermissions = func(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// do NOT check OPTIONS
-		if r.Method == "OPTIONS" {
-			next.ServeHTTP(w, r)
-			return
-		}
-
-		// allow everybody to auth
-		if strings.HasPrefix(r.URL.Path, "/api/auth") {
-			next.ServeHTTP(w, r)
-			return
-		}
-
 		// allow admin path only to admins
-		if strings.HasPrefix(r.URL.Path, "/api/admin") &&
-			r.Context().Value("context").(u.Values).Get("is_admin") == "false" {
-				u.HandleForbidden(w, errors.New("restricted access"))
-				return
+		if r.Context().Value("context").(u.Values).Get("is_admin") == "false" {
+			u.HandleForbidden(w, errors.New("restricted access"))
+			return
+		} else {
+			next.ServeHTTP(w, r)
 		}
-
-		next.ServeHTTP(w, r)
 	})
 }
